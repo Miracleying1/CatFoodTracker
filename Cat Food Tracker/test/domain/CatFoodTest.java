80d530e6df43204a8741203a3cc66dd8cdb1716f
jwang2 <jwang2@USEDPNB43260.insight.com>
2018-12-12 22:39:12 -0600

    added testing for Cat

--- Cat Food Tracker/test/domain/CatFoodTest.java
+++ Cat Food Tracker/test/domain/CatFoodTest.java
@@ -7,13 +7,6 @@ import org.junit.jupiter.api.Test;
 
 class CatFoodTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
--- Cat Food Tracker/test/domain/CatTest.java
+++ Cat Food Tracker/test/domain/CatTest.java
@@ -1,9 +1,17 @@
 package domain;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 class CatTest {
 
@@ -28,4 +36,40 @@ class CatTest {
 		assertEquals(cat.getTotalCaloriesToday(), 25);
 		assertEquals(cat.getRemainingCaloriesToday(), 275);
 	}
+	
+	@Test
+	void testGetFoodDiary() {
+		CatFood food=new CatFood("b","c","d",25);
+		FoodEntry entry = new FoodEntry(food, new Date(), 5);
+		FoodDiary diary=new FoodDiary();
+		diary.addEntry(entry);
+		assertNotEquals(diary.getEntries(), entry);
+		assertNotEquals(diary.getTodaysEntries(),entry);
+	}
+	
+	@Test
+	void testSetFoodDiary() {
+		CatFood food=new CatFood("b","c","d",25);
+		FoodEntry entry = new FoodEntry(food, new Date(), 5);
+		FoodDiary diary=new FoodDiary();
+		Map<Date, List<FoodEntry>> entries = new HashMap<Date, List<FoodEntry>>();
+		entries.get(entry);
+		diary.setEntries(entries);
+		assertEquals(diary.getEntries(),entries);
+	}
+
+	@Test
+	void testCatName() {
+		Cat cat=new Cat();
+		cat.setCatName("Fred");
+		assertEquals(cat.getCatName(),"Fred");
+	}
+	
+	@Test
+	void testCalorieGoal() {
+		Cat cat=new Cat();
+		cat.setCalorieGoal(300);
+		assertEquals(cat.getCalorieGoal(),300);
+	}
+
 }
